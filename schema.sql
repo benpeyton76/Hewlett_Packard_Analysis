588c9f74cf1fcc678644d24d4704ae033e056fd4
benpeyton76 <87077325+benpeyton76@users.noreply.github.com>
2021-10-27 18:25:34 -0500

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -16,6 +16,16 @@ CREATE TABLE employees (
     PRIMARY KEY (emp_no)
 );
 
+CREATE TABLE dept_managers (
+ 	dept_no VARCHAR NOT NULL,
+ 	emp_no INT NOT NULL,
+  	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  FOREIGN KEY (dept_no) REFERENCES 	departments (dept_no),	
+  PRIMARY KEY (emp_no, dept_no)
+);
+
 CREATE TABLE salaries (
 	emp_no INT NOT NULL,
   	salary INT NOT NULL,
@@ -25,31 +35,14 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_managers (
- 	dept_no VARCHAR(4) NOT NULL,
- 	emp_no INT NOT NULL,
-  	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
-);
-
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
-	dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
-);
-
-CREATE TABLE dept_emp (
-	emp_no INT NOT NULL,
-	dept_no VARCHAR(4) NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
@@ -58,5 +51,46 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no, title, from_date)
 );
+
+SELECT * FROM departments; 
+SELECT * FROM employees;
+SELECT * FROM dept_managers;
+SELECT * FROM salaries;
+SELECT * FROM dept_emp;
+SELECT * FROM titles;
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+--Retirement eligibility
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info
